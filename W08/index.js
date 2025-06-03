f64f5f0a62f1b6a50f69e214dbad5cc457be0073
RayZhou1191 <411411191@o365.tku.edu.tw>
2025-06-03 16:12:20 +0800

    message_for_update

--- W08/index.js
+++ W08/index.js
@@ -4,19 +4,16 @@ function openiframe(index) {
   switch (index) {
     case 1:
       c.innerHTML =
-        '<iframe src="/1132-web-411411191/W02/work.html" frameborder="1"></iframe>';
+        '<iframe src="../W02/HTML-work.html" frameborder="1"></iframe>';
       break;
     case 2:
-      c.innerHTML =
-        '<iframe src="/1132-web-411411191/W03/Card.html" frameborder="1"></iframe>';
+      c.innerHTML = '<iframe src="../W03/Card.html" frameborder="1"></iframe>';
       break;
     case 3:
-      c.innerHTML =
-        '<iframe src="/1132-web-411411191/W04/Blog.html" frameborder="1"></iframe>';
+      c.innerHTML = '<iframe src="../W04/Blog.html" frameborder="1"></iframe>';
       break;
     case 4:
-      c.innerHTML =
-        '<iframe src="/1132-web-411411191/W06/t3.html" frameborder="1"></iframe>';
+      c.innerHTML = '<iframe src="../W06/t3.html" frameborder="1"></iframe>';
       break;
   }
   console.log(index);
